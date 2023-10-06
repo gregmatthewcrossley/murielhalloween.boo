a06cd547c2841c4f77940af0777517968d397d9a
Greg Matthew Crossley <greg@crossley.to>
2023-10-06 14:00:48 -0400

    Rails and Ruby update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_29_130854) do
+ActiveRecord::Schema[7.1].define(version: 2023_10_06_175843) do
   create_table "trick_or_treaters", force: :cascade do |t|
     t.string "session_id", null: false
     t.string "sensitivity"
